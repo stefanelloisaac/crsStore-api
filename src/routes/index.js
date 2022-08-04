884f123c85ccdb939c7e73922b17b05547da6be4
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2022-08-04 20:12:41 -0300

    feat: gerador de pdf

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,8 @@
 import autorRoute from './autorRoute';
-import categoriasRoute from './categoriasRoute'
-import emprestimoRoute from './emprestimoRoute';
-import livroRoute from './livroRoute';
-import usuarioRoute from './usuarioRoute';
 
 
 function Routes(app){
-    categoriasRoute(app);
     autorRoute(app);
-    usuarioRoute(app);
-    livroRoute(app);
-    emprestimoRoute(app);
 }
 
 export default Routes
